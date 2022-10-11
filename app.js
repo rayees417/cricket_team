1a05a1d752f624878469cd7eb663c6c3953a3bf6
raiz <104497234+rayees417@users.noreply.github.com>
2022-10-11 19:57:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
-
 const app = express();
 app.use(express.json());
 const dbPath = path.join(__dirname, "cricketTeam.db");
